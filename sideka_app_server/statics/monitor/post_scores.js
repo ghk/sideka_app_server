a038c3f29a6646962dbafacba4f5e8196c1a4c53
Anton Maulana <anton_maulana@outlook.com>
2017-01-21 01:27:13 +0700

    improve post_scores

--- sideka_app_server/statics/monitor/post_scores.js
+++ sideka_app_server/statics/monitor/post_scores.js
@@ -10,17 +10,16 @@ app.controller('PostCtrl', function($scope, $http) {
 	$scope.keyWords = "";
 	$scope.totalItems = 0;
 	$scope.dataTables = [];
-	$scope.headers = ['Domain', 'Score', 'Title', 'Image?', '# Words', '# KBBI Words', '% KBBI','# Sentences', '# Paragraphs', 'Date', 'URL']
+	$scope.headers = ['Domain', 'Score', 'Title', 'Image?', '# Words', '# KBBI Words', '% KBBI','# Sentences', '# Paragraphs', 'Date']
 	get_all_post("",1)
 	
 	
 	$scope.changePage = function(page) {
 		get_all_post("",page)		
 	};
-	$scope.formatDate = function(date){
-		var dateOut = new Date(date);
-		return dateOut;
-	};
+ 	$scope.formatDate = function(date){           
+          return new Date(date);
+    };
 	function get_all_post(keyWords,pageBegin){
 		$http.get('/api/count_post_scores').then(function(response){
 			$scope.totalItems = parseInt(response.data)
