d7c395be4c3beef7f86bf3d0fd9f7480c4bf5dd0
Anton Maulana <anton_maulana@outlook.com>
2017-03-02 16:35:56 +0700

    using tab in apbdes

--- sideka_app_server/statics/monitor/apbdes_scores.js
+++ sideka_app_server/statics/monitor/apbdes_scores.js
@@ -1,56 +1,52 @@
-function makeLinkRenderer(link, text){
-	return function linkRenderer(instance, td, row, col, prop, value, cellProperties) {
-		td.innerHTML = "<a href='"+link(value)+"'>"+text(value)+"</a>";
-		return td;
-	}
+var header = ['Domain','Tahun','Pendapatan', 'Belanja', '# Baris','Last Modified' ];
+
+var getSubtype=function(data){
+	var unique=[];
+	data.map(function(c){
+		if(unique.indexOf(c.subtype)==-1 && c.subtype != "null")unique.push(c.subtype);		
+	});
+	return unique.sort();
 }
-var columns = [
-      {
-	data: 'domain',
-	header: 'Domain',
-	renderer: makeLinkRenderer(function(v) { return "http://"+v}, function(v) { return v; }),
-      },
-      {	
-	data: 'subtype',
-	header: 'Tahun',
-      },
-      {	data: 'pendapatan',
-	header: 'Pendapatan',
-	type: 'numeric',
-	format: '0,0',
-      },
-      {	
-	data: 'belanja',
-	header: 'Belanja',
-	type: 'numeric',
-	format: '0,0',
-      },
-      {	
-	data: 'rows',
-	header: '# Baris',
-      },
-      {
-	data: 'last_modified',
-	header: 'Last Modified',
-      },
-    ];
-columns.forEach(function(c) {
-	c.readOnly = true;
-});
+var currencyConvert = function (n, currency) {
+    return currency + " " + n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+}
+
+var applyContent = function(data,subtype){
+	$("#years-apbdes >li").remove();
+	$("#content-apbdes >li").remove();
+
+	for(var i=0;i < subtype.length;i++){
+		var tableContent = $("#table-content").clone().removeClass("hidden");
+		var tbody = $("tbody", tableContent);
+		var dataFiltered = data.filter(c=>c.subtype == subtype[i])		
+		var li = $('<li>');
+
+		$('<a>').attr("href","").append(subtype[i]).appendTo(li)
+		if (i ==0)li.addClass("uk-active");
+		$("#years-apbdes").append(li);			
+		$.each(dataFiltered,function(idx, row){
+			var tr = $('<tr>');
+			$('<td>').html('<a href="http://'+row.domain+'">'+row.domain+'</a>').appendTo(tr);
+			$('<td>').html(currencyConvert(row.pendapatan, "Rp")).appendTo(tr);
+			$('<td>').html(currencyConvert(row.belanja,"Rp")).appendTo(tr);
+			$('<td>').html(row.rows).appendTo(tr);
+			$('<td>').html(row.last_modified).appendTo(tr);
+			tbody.append(tr);
+		})	 
+		li = $('<li>').append(tableContent);
+		if(i==0)li.addClass("uk-active");
+		$("#content-apbdes").append(li);
+	}	
+}
+
 var changeSelected = function(supradesa_id){
 	$.getJSON("/api/apbdes_scores?supradesa_id="+supradesa_id, function(data){
-		var container = document.getElementById('sheet');
-		var hot = new Handsontable(container, {
-		data: data.filter(function(d) { return d.pendapatan && d.belanja; }),
-			columns: columns,
-			columnSorting: true,
-			sortIndicator: true,
-			rowHeaders: true,
-			colHeaders: columns.map(function(c) {return c.header;}),
-		});
-		setTimeout(function(){ hot.render()}, 0);
+		var subtype = getSubtype(data)
+		applyContent(data,subtype)
 	});
 }
+
+
 changeSelected(hashUrl());
 $('#select-supradesa').change(function(){
 	var value = $(this).val();
--- sideka_app_server/statics/monitor/statistics.js
+++ sideka_app_server/statics/monitor/statistics.js
@@ -1,68 +1,156 @@
-var header = ['Wp Id','Domain','Berita', 'Penduduk','Anggaran','B. Qlt','B. Freq','P. Qlt', 'P. Qty', 
-'A. Qlt','A. Qty','Last Post','#Post 1D', '#Post 1W', '#Post 30D','Penduduk Last Modified', '# Penduduk', 'APBDes Last Modified','# APBDes',]
-var applyHeaderandFooter =function(){
-	$('#table-data-quality thead > tr').remove()
-	var thead = $('#table-data-quality thead');
-	var tr = $('<tr>');
-	for(var i = 0;i < header.length;i++){
-		$('<th>').html(header[i]).appendTo(tr);		
+function makeLinkRenderer(link, text){
+	return function linkRenderer(instance, td, row, col, prop, value, cellProperties) {
+		td.innerHTML = "<a href='"+link(value)+"'>"+text(value)+"</a>";
+		return td;
 	}
-	thead.append(tr);
 }
 
-var makeButtonScoring = function(score){
-	var classButton;
-	score *=100;	
-	if(score < 40)
-		classButton= 'uk-badge uk-badge-danger';
-	else if(score < 70) 
-		classButton ='uk-badge uk-badge-warning'; 
-	else
-		classButton ='uk-badge uk-badge-success';	
-	return '<span class="'+classButton+'">'+score.toFixed(2)+'</span>';;
-}
+var scoreRenderer = function(instance, td, row, col, prop, value, cellProperties) {
+	var args = [instance, td, row, col, prop, value * 100, cellProperties];
+	var bg = "red";
+	var color = "white";
+	if (value > 0.4){
+		bg = "yellow";
+		color = "black";
+	}
+	if (value > 0.7){
+		bg = "green";
+		color = "white";
+	}
 
-var changeSelected = function(supradesa_id){	
-	$('#table-data-quality thead > tr').remove()
-	$('#table-data-quality tbody > tr').remove()
+	Handsontable.renderers.NumericRenderer.apply(this, args);
+	td.style.backgroundColor = bg;
+	td.style.color = color;
+};
 
-	applyHeaderandFooter();
+var columns = [
+		{
+	data: 'blog_id',
+	header: 'Wordpress Id',
+	renderer: makeLinkRenderer(function(v){ return  "/statistic/"+v }, function(v) {return v; }),
+		},
+		{
+	data: 'domain',
+	header: 'Domain',
+	renderer: makeLinkRenderer(function(v){ return  "http://"+v }, function(v) {return v; }),
+      },
+/*
+      {
+	data: 'pendamping',
+	header: 'Pendamping',
+      },
+*/
+      {
+	data: 'blog.score',
+	header: 'Berita',
+	type: 'numeric',
+	format: '0.00',
+	renderer: scoreRenderer
+      },
+      {
+	data: 'penduduk.score',
+	header: 'Penduduk',
+	type: 'numeric',
+	format: '0.00',
+	renderer: scoreRenderer
+      },
+      {
+	data: 'apbdes.score',
+	header: 'Anggaran',
+	type: 'numeric',
+	format: '0.00',
+	renderer: scoreRenderer
+      },
+      {
+	data: 'blog.score_quality',
+	header: 'B. Qlt',
+	type: 'numeric',
+	format: '0.00',
+      },
+      {
+	data: 'blog.score_frequency',
+	header: 'B. Freq',
+	type: 'numeric',
+	format: '0.00',
+      },
+      {
+	data: 'penduduk.score_quality',
+	header: 'P. Qlt',
+	type: 'numeric',
+	format: '0.00',
+      },
+      {
+	data: 'penduduk.score_quantity',
+	header: 'P. Qty',
+	type: 'numeric',
+	format: '0.00',
+      },
+      {
+	data: 'apbdes.score_quality',
+	header: 'A. Qlt',
+	type: 'numeric',
+	format: '0.00',
+      },
+      {
+	data: 'apbdes.score_quantity',
+	header: 'A. Qty',
+	type: 'numeric',
+	format: '0.00',
+      },
+      {
+	data: 'blog.last_post',
+	header: 'Last Post',
+      },
+      {
+	data: 'blog.count_24h',
+	header: '#Post 1D',
+      },
+      {
+	data: 'blog.count_1w',
+	header: '#Post 1W',
+      },
+      {
+	data: 'blog.count_30d',
+	header: '#Post 30D',
+      },
+      {
+	data: 'penduduk.last_modified',
+	header: 'Penduduk Last Modified',
+      },
+      {
+	data: 'penduduk.count',
+	header: '# Penduduk',
+      },
+      {
+	data: 'apbdes.last_modified',
+	header: 'APBDes Last Modified',
+      },
+      {
+	data: 'apbdes.count',
+	header: '# APBDes',
+      },
+    ];
+columns.forEach(function(c) {
+	c.readOnly = true;
+});
+var changeSelected = function(supradesa_id){
 	$.getJSON("/api/statistics?supradesa_id="+supradesa_id, function(data){
-		var tbody = $('#table-data-quality tbody');
-		var origin = window.location.origin;
-		var pathName = window.location.pathname;	
-		var url = origin +"/statistic/";
-		$.each(data,function(idx,content){
-			var tr = $('<tr>');
-			$('<td>').html('<a href="'+ url +content.blog_id+'">'+content.blog_id+'</a>').appendTo(tr);
-			$('<td>').html('<a href="http://'+content.domain+'">'+content.domain+'</a>').appendTo(tr);
-			$('<td class="uk-text-center">').html(makeButtonScoring(content.blog.score)).appendTo(tr);
-			$('<td class="uk-text-center">').html(makeButtonScoring(content.penduduk.score)).appendTo(tr);
-			$('<td class="uk-text-center">').html(makeButtonScoring(content.apbdes.score)).appendTo(tr);
-			$('<td>').html((content.blog.score_quality *100).toFixed(2)).appendTo(tr);
-			$('<td>').html((content.blog.score_frequency *100).toFixed(2)).appendTo(tr);
-			$('<td>').html((content.penduduk.score_quality *100).toFixed(2)).appendTo(tr);
-			$('<td>').html((content.penduduk.score_quantity *100).toFixed(2)).appendTo(tr);
-			$('<td>').html((content.apbdes.score_quality *100).toFixed(2)).appendTo(tr);
-			$('<td>').html((content.apbdes.score_quantity *100).toFixed(2)).appendTo(tr);
-			$('<td>').html(content.blog.last_post).appendTo(tr);
-			$('<td>').html(content.blog.count_24h).appendTo(tr);
-			$('<td>').html(content.blog.count_1w).appendTo(tr);
-			$('<td>').html(content.blog.count_30d).appendTo(tr);
-			$('<td>').html(content.penduduk.last_modified).appendTo(tr);
-			$('<td>').html(content.penduduk.count).appendTo(tr);			
-			$('<td>').html(content.apbdes.last_modified).appendTo(tr);	
-			$('<td>').html(content.apbdes.count).appendTo(tr);		
-			tbody.append(tr);
-			
-		})
-	})
+		var container = document.getElementById('sheet');
+
+		var hot = new Handsontable(container, {
+		data: data,
+		columns: columns,
+		columnSorting: true,
+		sortIndicator: true,
+		rowHeaders: true,
+		colHeaders: columns.map(function(c) { return c.header; }),
+		});
+		setTimeout(function(){ hot.render() }, 0);
+	});	
 }
 changeSelected(hashUrl());
 $('#select-supradesa').change(function(){
 	var value = $(this).val();
-	changeSelected(value)
+	changeSelected(value) 
 	changeUrl(value) 
 });
-
-
