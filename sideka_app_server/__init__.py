1d3e29cfe38810a3413ee2571a72c35d65ab8aca
wm-mrx <wm.mrx2015@gmail.com>
2017-07-27 17:59:04 +0700

    update

--- sideka_app_server/__init__.py
+++ sideka_app_server/__init__.py
@@ -62,7 +62,7 @@ def login():
         logs(user_id, desa_id, token, "login", None, None)
         return jsonify({'success': success, 'desa_id': desa_id, 'desa_name': desa_name, 'token': token , 'user_id': user_id, 'user_nicename': user_nicename, 'api_version': app.config["API_VERSION"]})
     except Exception:
-        return jsonify({success: False, message: Exception}), 500
+        return jsonify({"success": False, "message": Exception}), 500
     finally:
         cur.close()
 
@@ -166,10 +166,10 @@ def post_content(desa_id, content_type, content_subtype = None):
         cur.execute(max_change_id_query, (content_type, content_subtype, desa_id))
         cur_fetch_max_change_id = cur.fetchone()
 
-        if cur_fetch_max_change_id is None:
-            return jsonify({}), 404
-        
-        max_change_id = int(cur_fetch_max_change_id[0])
+        if cur_fetch_max_change_id[0] is None:
+            mex_change_id = 0
+        else:
+            max_change_id = int(cur_fetch_max_change_id[0])
 
         if content_type == 'subtypes':
             return jsonify({success: False}), 500
@@ -223,7 +223,7 @@ def get_content_v2(desa_id, content_type, content_subtype = None):
         api_version = cursor[2]
         
         if api_version != app.config["API_VERSION"]:
-            new_content = {"changeId": 0, "data": [], "columns": [],  "apiVersion": app.config["API_VERSION"]}
+            new_content = {"changeId": 0, "data": [], "columns": [], "apiVersion": app.config["API_VERSION"]}
 
             for index, data in enumerate(content["data"]):
                if isinstance(content["data"][index], list):
@@ -269,12 +269,12 @@ def post_content_v2(desa_id, content_type, content_subtype = None):
             max_change_id_query = "SELECT MAX(change_id) FROM sd_contents WHERE type=%s AND subtype is %s AND desa_id=%s"
         
         cur.execute(max_change_id_query, (content_type, content_subtype, desa_id))
-        cursor_max_change_id = cur.fetchone()
-
-        if cursor_max_change_id is None:
-            return jsonify({}), 404
-        
-        max_change_id = int(cursor_max_change_id[0])
+        cursor_max_change_id = cur.fetchone() 
+   
+        if cursor_max_change_id[0] is None: 
+            max_change_id = 0
+        else:
+            max_change_id = int(cursor_max_change_id[0])
 
         contents_query = "SELECT content FROM sd_contents WHERE type=%s AND subtype=%s AND desa_id=%s AND change_id > %s ORDER BY change_id ASC"
 
@@ -317,112 +317,46 @@ def post_content_v2(desa_id, content_type, content_subtype = None):
         cur.execute(current_content_query, (content_type, content_subtype, desa_id, client_change_id))
         cursor_current_content = cur.fetchone()
         
+        #if there is no data in server at all
         if cursor_current_content is None:
-            return jsonify({}), 404
-        
-        current_content = json.loads(cursor_current_content[0])
-        
-        if isinstance(current_content["data"], list):
-            new_content["data"]["penduduk"] = merge_diffs(new_content["diffs"]["penduduk"], current_content["data"])
-        elif isinstance(current_content["data"], dict):
-            for key, value in request.json["columns"].items():
-                new_content["data"][key] = merge_diffs(new_content["diffs"][key], current_content["data"][key])
+           for key, value in request.json["columns"].items():   
+               if content_type == 'map':
+                   new_content['data'][key] = merge_map_diffs(new_content['diffs'][key], [])
+               else:
+                   new_content['data'][key] = merge_diffs(new_content['diffs'][key], [])
+
+               new_content['diffs'][key] = [] 
+        else:
+            current_content = json.loads(cursor_current_content[0])
+            
+            if isinstance(current_content["data"], list):
+                new_content["data"]["penduduk"] = merge_diffs(new_content["diffs"]["penduduk"], current_content["data"])
+            elif isinstance(current_content["data"], dict):
+                for key, value in request.json["columns"].items():
+                    if content_type != 'map':
+                        if len(new_content["diffs"][key]) > 0:
+                            new_content["data"][key] = merge_diffs(new_content["diffs"][key], current_content["data"][key])
+                        else:
+                            new_content["data"][key] = current_content["data"][key]
+
+                        new_content["columns"][key] = request.json["columns"][key]
+                    elif content_type == 'map':
+                        if len(new_content["diffs"][key]) > 0:
+                            new_content["data"][key] = merge_map_diffs(new_content["diffs"][key], current_content["data"][key])
+                        else:
+                            new_content["data"][key] = current_content["data"][key]
         
         cur.execute("INSERT INTO sd_contents(desa_id, type, subtype, content, date_created, created_by, change_id, api_version) VALUES(%s, %s, %s, %s, now(), %s, %s, %s)", (desa_id, content_type, content_subtype, json.dumps(new_content), user_id, new_change_id, app.config["API_VERSION"]))
-        mysql.connection.commit()
+        mysql.connection.commit()    
         logs(user_id, desa_id, "", "save_content", content_type, content_subtype)
         suceess = True
+
+        if cursor_current_content is None:
+            return jsonify({"success": True, "change_id": new_change_id, "diffs": {}, "data": new_content['data']})
+
         return jsonify({"success": True, "change_id": new_change_id, "diffs": diffs })
-    except Exception, e:
-        return jsonify({"message": str(e)}), 500
     finally:
         cur.close()
-
-@app.route('/content-map/<int:desa_id>', methods=["GET"])
-@app.route('/content-map/<int:desa_id>/<int:change_id>', methods=["GET"])
-def get_map_content(desa_id, change_id = None):
-	cur = mysql.connection.cursor()
-	try:
-		user_id = get_auth(desa_id, cur)
-		result = None
-		
-		if user_id is None:
-			return jsonify({}), 403
-		
-		if change_id is None:
-			change_id = 0
-
-		content_query = "SELECT content, change_id FROM sd_contents where type='map' AND desa_id=%s AND change_id >= %s ORDER BY change_id DESC"
-		cur.execute(content_query, (desa_id, change_id))
-		cur_bundle_data = cur.fetchone()
-			
-		if cur_bundle_data is None:
-			return jsonify({}), 404
-		
-		bundle_data = json.loads(cur_bundle_data[0])
-
-		diffs = []
-
-		if bundle_data.has_key("diffs"):
-			diffs = bundle_data["diffs"]
-
-		if change_id > 0:
-			return jsonify({"change_id": cur_bundle_data[1], "diffs": diffs })	
-
-		return jsonify({"change_id": cur_bundle_data[1], "center": bundle_data["center"], "data": bundle_data["data"] })
-	finally:
-		cur.close()
-
-@app.route('/content-map/<int:desa_id>', methods=["POST"])
-@app.route('/content-map/<int:desa_id>/<int:change_id>', methods=["POST"])
-def post_map_content(desa_id, change_id = None):
-	 cur = mysql.connection.cursor()
-	 try:
-		user_id = get_auth(desa_id, cur)
-		result = None
-		
-		if user_id is None:
-			return jsonify({}), 403
-		
-		max_change_id_query = "SELECT MAX(change_id) FROM sd_contents WHERE type='map' AND desa_id=%s"
-		cur.execute(max_change_id_query, (desa_id, ))
-		cur_fetch_max_change_id = cur.fetchone()
-
-		if cur_fetch_max_change_id is None:
-			return jsonify({}), 404
-		
-		max_change_id = int(cur_fetch_max_change_id[0])
-		latest_content_query = "SELECT content FROM sd_contents WHERE type='map' AND desa_id=%s AND change_id > %s ORDER BY change_id ASC"
-		cur.execute(latest_content_query, (desa_id, change_id))
-		cur_fetch_latest_contents = cur.fetchall()
-		diffs = []
-
-		for cur_fetch_latest_content in cur_fetch_latest_contents:
-			latest_content = json.loads(cur_fetch_latest_contents[0])
-
-			if latest_content.has_key("diffs") == False:
-				continue
-			
-			diffs.append(latest_content["diffs"])
-		
-		new_change_id = max_change_id + 1
-		current_content_query = "SELECT content FROM sd_contents WHERE type='map' AND desa_id=%s AND change_id=%s"
-		cur.execute(current_content_query, (desa_id, change_id))
-		cur_fetch_current_content = cur.fetchone()	
-
-		if cur_fetch_current_content is None:
-			return jsonify({}), 404
-			
-		current_content = json.loads(cur_fetch_current_content[0])
-		new_content = {"changeId": new_change_id, "center": current_content["center"], "data": [], "diffs": request.json["diffs"]}
-		new_content["data"] = merge_map_diffs(new_content["diffs"], current_content["data"])
-		json_new_content = json.dumps(new_content)
-		cur.execute("INSERT INTO sd_contents(desa_id, type, subtype, content, date_created, created_by, change_id) VALUES(%s, %s, %s, %s, now(), %s, %s)", (desa_id, 'map', None, json_new_content, user_id, new_change_id))
-		mysql.connection.commit()
-		suceess = True
-		return jsonify({"success": True, "change_id": new_change_id, "diffs": diffs })
-	 finally:
-		 cur.close()
 @app.route('/desa', methods=["GET"])
 @cross_origin()
 def get_all_desa():
@@ -437,32 +371,29 @@ def get_all_desa():
 
 def merge_diffs(diffs, data):
     for diff in diffs:
-            for add in diff["added"]:
-                data.append(add)
-            for modified in diff["modified"]:
-                for index, item in enumerate(data):
-                    if item[0] == modified[0]:
-                        data[index] = modified
-                        break
-            for deleted in diff["deleted"]:
-                for item in data:
-                    if item[0] == deleted[0]:
-                        data.remove(item)
-                        break;
+        for add in diff["added"]:
+            data.append(add)
+        for modified in diff["modified"]:
+            for index, item in enumerate(data):
+                if item[0] == modified[0]:
+                    data[index] = modified
+        for deleted in diff["deleted"]:
+            for item in data:
+                if item[0] == deleted[0]:
+                    data.remove(item)
     return data
 def merge_map_diffs(diffs, data):
-    	for diff in diffs:
-		for added in diff["added"]:
-			data.append(added)
-		for modified in diff["modified"]:
-			for index, item in enumerate(data):
-				if item["id"] == modified["id"]:
-					data[index] = modified
-					break
-		for deleted in diff["deleted"]:
-			for item in data:
-				if item["id"] == deleted["id"]:
-					data.remove(item)
+    for diff in diffs:
+        for added in diff["added"]:
+            data.append(added)
+        for modified in diff["modified"]:
+            for index, item in enumerate(data):
+                if item["id"] == modified["id"]:
+                    data[index] = modified
+        for deleted in diff["deleted"]:
+            for item in data:
+                if item["id"] == deleted["id"]:
+                    data.remove(item)
 	return data
 def get_auth(desa_id, cur):
     token = request.headers.get('X-Auth-Token', None)
@@ -485,173 +416,21 @@ def logs(user_id, desa_id, token, action, content_type, content_subtype):
 	mysql.connection.commit()
 	cur.close();
 
-#deprecated
-@app.route('/content/2.0/<int:desa_id>/<content_type>/<key>', methods=["GET"])
-@app.route('/content/2.0/<int:desa_id>/<content_type>/<key>/<content_subtype>', methods=["GET"])
-def get_content_2_0(desa_id, content_type, key, content_subtype=None):
-    cur = mysql.connection.cursor()
-    result = None
+
+@app.route('/content-map/v2/<int:desa_id>/<indicator>', methods=['POST'])
+def upload_map_indicator(desa_id, indicator):
     try:
-			user_id = get_auth(desa_id, cur)
-			change_id = int(request.args.get("changeId", "0"))
+        geojson = request.json["data"]
+        result = []
 
-			if user_id is None:
-				return jsonify({}), 403
-			
-			content_query = "SELECT content, change_id, api_version FROM sd_contents WHERE type=%s AND subtype=%s AND desa_id=%s AND change_id >=%s ORDER BY change_id DESC"
-			
-			if content_subtype is None:
-				content_query = "SELECT content, change_id, api_version FROM sd_contents WHERE type=%s AND subtype is %s AND desa_id=%s AND change_id >=%s ORDER BY change_id DESC"
-		
-			cur.execute(content_query, (content_type, content_subtype, desa_id, change_id))
-			cur_fetch = cur.fetchone()
-			
-			if cur_fetch is None:
-				return jsonify({}), 404
-			
-			bundle_data = json.loads(cur_fetch[0])
+        for data in geojson['features']:
+            data['id'] =base64.urlsafe_b64encode(uuid.uuid4().bytes).strip("=")
+            data['indicator'] = indicator
+            data['properties'] = {'type': None}
+            result.append(data)
 
-			diffs = {}
-			diffs[key] = []
-		
-			current_change_id = cur_fetch[1]
-			api_version = cur_fetch[2]
-
-			if api_version != app.config["API_VERSION"]:
-				bundle_data_new = []
-				for index, data in enumerate(bundle_data["data"]):
-					if isinstance(bundle_data["data"][index], list):				
-						bundle_data["data"][index].insert(0,  base64.urlsafe_b64encode(uuid.uuid4().bytes).strip("="))
-						bundle_data_new.append(bundle_data["data"][index])
-						
-				bundle_data["data"] = bundle_data_new
-				api_version = app.config["API_VERSION"]
-				current_change_id = current_change_id + 1
-				query_insert = 'INSERT INTO sd_contents(desa_id, type, subtype, content, date_created, created_by, change_id, api_version) VALUES(%s, %s, %s, %s, now(), %s, %s, %s)'
-				cur.execute(query_insert, (desa_id, content_type, content_subtype, json.dumps(bundle_data), user_id, current_change_id, app.config["API_VERSION"]))
-				mysql.connection.commit()
-			
-			if bundle_data.has_key("diffs") == False:
-				return jsonify({"change_id": current_change_id, "data": bundle_data["data"], "api_version": api_version })
-			
-			elif bundle_data["diffs"].has_key(key):
-				diffs[key] = bundle_data["diffs"][key]
-			
-			if change_id > 0:
-				return jsonify({"change_id": current_change_id, "diffs": diffs[key], "api_version": api_version })
-			
-			return jsonify({"change_id": current_change_id, "data": bundle_data["data"], "api_version": api_version })
-
+        return jsonify({"success": True, "data": result })
     finally:
-        cur.close()
-
-#deprecated
-@app.route('/content/2.0/<int:desa_id>/<content_type>/<key>', methods=["POST"])
-@app.route('/content/2.0/<int:desa_id>/<content_type>/<key>/<content_subtype>', methods=["POST"])
-def post_content_2_0(desa_id, content_type, key, content_subtype=None):
-    cur = mysql.connection.cursor()
-	
-    try:
-		print request.json
-		user_id = get_auth(desa_id, cur)
-		current_change_id = int(request.args.get("changeId", 0))
-		result = None
-
-		if user_id is None or current_change_id is None:
-			return jsonify({}), 403
-		
-		max_change_id_query = "SELECT MAX(change_id) FROM sd_contents WHERE type=%s AND subtype=%s AND desa_id=%s"
-
-		if content_subtype is None:
-			max_change_id_query = "SELECT MAX(change_id) FROM sd_contents WHERE type=%s AND subtype is %s AND desa_id=%s"
-		
-		cur.execute(max_change_id_query, (content_type, content_subtype, desa_id))
-		cur_fetch_max_change_id = cur.fetchone()
-
-		if cur_fetch_max_change_id is None:
-			return jsonify({}), 404
-		
-		max_change_id = int(cur_fetch_max_change_id[0])
-
-		latest_content_query = "SELECT content FROM sd_contents WHERE type=%s AND subtype=%s AND desa_id=%s AND change_id > %s ORDER BY change_id ASC"
-
-		if content_subtype is None:
-			latest_content_query = "SELECT content FROM sd_contents WHERE type=%s AND subtype is %s AND desa_id=%s AND change_id > %s ORDER BY change_id ASC"
-		
-		cur.execute(latest_content_query, (content_type, content_subtype, desa_id, current_change_id))
-		cur_fetch_latest_contents = cur.fetchall()
-
-		diffs = {}
-		diffs[key] = []
-
-		for cur_fetch_latest_content in cur_fetch_latest_contents:
-			latest_content = json.loads(cur_fetch_latest_contents[0])
-			if latest_content.has_key("diffs") == False:
-				continue
-			
-			if latest_content["diffs"].has_key(key):
-				for diff in latest_content["diffs"][key]:
-						diffs[key].append(diff)
-		
-		if content_subtype != 'subtype':
-			new_change_id = max_change_id + 1
-			current_content_query = "SELECT content FROM sd_contents WHERE type=%s AND subtype=%s AND desa_id=%s AND change_id=%s"
-
-			if content_subtype is None:
-				current_content_query = "SELECT content FROM sd_contents WHERE type=%s AND subtype is %s AND desa_id=%s AND change_id=%s"
-			
-			cur.execute(current_content_query, (content_type, content_subtype, desa_id, current_change_id))
-			cur_fetch_current_content = cur.fetchone()	
-
-			if cur_fetch_current_content is None:
-				return jsonify({}), 404
-			
-			current_content = json.loads(cur_fetch_current_content[0])
-			current_content_data = []
-			current_content_columns = []
-
-			if isinstance(current_content["data"], list) and key == 'penduduk':
-				current_content_data = current_content["data"]
-			elif isinstance(current_content["data"], dict) and current_content["data"].has_key(key):
-				current_content_data = current_content["data"][key]
-			
-			if isinstance(current_content["columns"], list) and key == 'penduduk':
-				current_content_columns = current_content["columns"]
-			elif isinstance(current_content["columns"], dict) and current_content["columns"].has_key(key):
-				current_content_columns = current_content["columns"][key]
-
-			if current_content_columns == []:
-				current_content_columns = request.json["columns"]
-			
-			new_content = {"changeId": new_change_id, "columns": {}, "data": {}, "diffs": {}}
-			new_content["columns"][key] = current_content_columns
-			new_content["data"][key] = merge_diffs(request.json["diffs"], current_content_data)
-			new_content["diffs"][key] = request.json["diffs"]
-
-			if isinstance(current_content["data"], list) and key != "penduduk":
-			   new_content["data"]["penduduk"] = current_content["data"]
-			
-			if isinstance(current_content["columns"], list) and key != "penduduk":
-				new_content["columns"]["penduduk"] = current_content["columns"]
-			
-			if isinstance(current_content["data"], dict) and isinstance(current_content["columns"], dict):
-				content_keys = current_content.keys()
-
-				for content_key in content_keys:
-					if isinstance(current_content[content_key], dict):
-						data_keys = current_content[content_key].keys()
-						for content_data_key in data_keys:
-							if content_data_key != key:
-								new_content["data"][content_data_key] = current_content["data"][content_data_key]
-								new_content["columns"][content_data_key] = current_content["columns"][content_data_key]
-
-			json_new_content = json.dumps(new_content)
-			cur.execute("INSERT INTO sd_contents(desa_id, type, subtype, content, date_created, created_by, change_id, api_version) VALUES(%s, %s, %s, %s, now(), %s, %s, %s)", (desa_id, content_type, content_subtype, json_new_content, user_id, new_change_id, app.config["API_VERSION"]))
-			mysql.connection.commit()
-			logs(user_id, desa_id, "", "save_content", key, content_subtype)
-			suceess = True
-			return jsonify({"success": True, "change_id": new_change_id, "diffs": diffs[key] })
-    finally:
-        cur.close()
+        print "close"
 if __name__ == '__main__':
     app.run(debug=True, host=app.config["HOST"], port=app.config["PORT"])
