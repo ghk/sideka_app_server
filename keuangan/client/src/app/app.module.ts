05f697793846d32412acfffad1fbb3579f022c0a
Vidyapati <vidyapati@microvac.co.id>
2017-08-27 21:14:19 +0700

    Keuangan barebone

--- keuangan/client/src/app/app.module.ts
+++ keuangan/client/src/app/app.module.ts
@@ -4,19 +4,30 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app';
+import { HeaderComponent } from './components/header';
+import { TransferRecapitulationComponent } from './components/transfer-recapitulation';
+
+import { DataService } from './services/data';
+import { SharedService } from './services/shared';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TransferRecapitulationComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot([
-      { path: '', redirectTo: '', pathMatch: 'full' }
+      { path: '', redirectTo: '', pathMatch: 'full' },
+      { path: 'transfer', component: TransferRecapitulationComponent}
     ])
   ],
-  providers: [],
+  providers: [
+    DataService,
+    SharedService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- keuangan/client/src/app/components/header.ts
+++ keuangan/client/src/app/components/header.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
-  selector: 'sk-app',
-  templateUrl: '../templates/app.html',
+  selector: 'sk-header',
+  templateUrl: '../templates/header.html',
 })
 
-export class AppComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void { }
-
+  
   ngOnDestroy(): void { }
 }
