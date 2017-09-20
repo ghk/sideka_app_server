769ad02c493d69014ab57e25a02d8f088136e299
Gozali Kumara <ghk@microvac.co.id>
2017-09-20 12:10:56 +0700

    merge data directly

--- api/api.py
+++ api/api.py
@@ -366,14 +366,16 @@ def post_content_v2(desa_id, content_type, content_subtype=None):
                     current_content["data"][key]=[]
 
                 if(len(new_content["diffs"][key]) > 0):
-                   merge_method = merge_map_diffs if content_type == 'pemetaan' else merge_diffs
-                   new_content["data"][key] = merge_method(new_content["diffs"][key], current_content["data"][key])
+                    merge_method = merge_map_diffs if content_type == 'pemetaan' else merge_diffs
+                    new_content["data"][key] = merge_method(new_content["diffs"][key], current_content["data"][key])
                 else:
                     new_content["data"][key] = current_content["data"][key]
 
-                new_content["data"][key] = current_content["data"][key]
+                if "data" in request.json and key in request.json["data"] and content_type in ["perencanaan", "penganggaran", "penerimaan", "spp"]:
+                    new_content["data"][key] = request.json["data"][key]
 
-        cur.execute("INSERT INTO sd_contents(desa_id, type, subtype, content, date_created, created_by, change_id, api_version) VALUES(%s, %s, %s, %s, now(), %s, %s, %s)", (desa_id, content_type, content_subtype, json.dumps(new_content), user_id, new_change_id, app.config["API_VERSION"]))
+        cur.execute("INSERT INTO sd_contents(desa_id, type, subtype, content, date_created, created_by, change_id, api_version) VALUES(%s, %s, %s, %s, now(), %s, %s, %s)", 
+            (desa_id, content_type, content_subtype, json.dumps(new_content), user_id, new_change_id, app.config["API_VERSION"]))
         mysql.connection.commit()    
         logs(user_id, desa_id, "", "save_content", content_type, content_subtype)
         return_data['success'] = True
