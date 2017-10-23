00086a324b86f0738c09be9b1fda39df01c6c406
wm-mrx <wm.mrx2015@gmail.com>
2017-10-23 15:50:15 +0700

    mapLayout and mapData

--- tatakelola/client/src/app/components/desa.ts
+++ tatakelola/client/src/app/components/desa.ts
@@ -13,13 +13,16 @@ import MapUtils from '../helpers/mapUtils';
      selector: 'desa',
      templateUrl: '../templates/desa.html'
 })
-
 export class DesaComponent implements OnInit, OnDestroy { 
     leafletOptions: any;
     isStatisticShown: boolean;
     isSidebarShown: boolean;
     geojsonData: L.GeoJSON;
+    geojsonOptions: any;
     map: L.Map;
+    mapData: any;
+    mapLayout: any[];
+    bigConfig: any;
 
     constructor(private http:Http) {}
 
@@ -33,31 +36,128 @@ export class DesaComponent implements OnInit, OnDestroy {
             zoom: 14,
             center: L.latLng([-7.547389769590928, 108.21044272398679])
         }      
+  
+       this.geojsonOptions = {
+          style: (feature) => {
+              return { color: '#000', weight: feature.geometry.type === 'LineString' ? 3 : 1 }
+          },
 
-        $('.leaflet-control-zoom').css({display: 'none'});
+          onEachFeature: (feature, layer) => {
+              let indicators = this.bigConfig;
+
+              for(let i=0; i<indicators.length; i++) { 
+                  let indicator = indicators[i];
+                  let element = null;
+
+                  for(let j=0; j<indicator.elements.length; j++) {
+                      let current = indicator.elements[j];
+
+                    if(current.values){
+                        let valueKeys = Object.keys(current.values);
+                        if(valueKeys.every(valueKey => feature["properties"][valueKey] === current.values[valueKey])){
+                            element = current;
+                            break;
+                        }
+                    }
+                  }
+
+                  if (!element)
+                    return;
+
+                  if (element['style']) {
+                    let style = MapUtils.setupStyle(element['style']);
+                    layer.setStyle(style);
+                  }
+              }
+          }
+       }
+
+       this.loadBigConfig();
+       this.loadMapData();
+       this.loadMapLayout();
+
+       $('.leaflet-control-zoom').css({display: 'none'});
+    }
+    
+    loadMapLayout(): void {
+        this.http.get('assets/mapLayout.json').map(res => res.json()).subscribe(
+           result => {
+              this.mapLayout = result;
+           },
+           error => {}
+        )
+    }
+
+    loadMapData(): void {
+        this.http.get('assets/mapData.json').map(res => res.json()).subscribe(
+           result => {
+              this.mapData = result;
+           },
+           error => {}
+        )
+    }
+
+    loadBigConfig(): void {
+        this.http.get('assets/bigConfig.json').map(res => res.json()).subscribe(
+            result => {
+               this.bigConfig = result;
+            },
+            error => {}
+        );
+    }
+
+    //Dummy method to get map layout
+    extractData(): void {
+       this.http.get('assets/pemetaan.json').map(res => res.json()).subscribe(
+          result => {
+             let indicators = Object.keys(result.data).filter(e => e !== 'log_pembangunan');
+             let mapLayout = [];
+
+             indicators.forEach(indicator => {
+                 mapLayout = mapLayout.concat(result.data[indicator]);
+             });
+          },
+          error => {}
+       )
     }
 
     ngOnDestroy(): void {}
 
     onMapReady(map: L.Map): void {
        this.map = map;
-       this.loadMapData();
     }
 
-    loadMapData(): void {
-       this.http.get('assets/data.json').map(res => res.json()).subscribe(
-           result => {
-               let data = MapUtils.createGeoJson();
-               data.features.push(result.batas.desa[0]);
+    setMapData(type): boolean {
+       this.clearMap();
+       let geojson = MapUtils.createGeoJson();
 
-               let options = {
-                 style: (feature) => {
-                      return { color: '#000', weight: feature.geometry.type === 'LineString' ? 3 : 1 }
-                  },
-               };
-               this.geojsonData = L.geoJSON(data, options).addTo(this.map);
-           },
-           error => {}
-       )
+       switch(type) {
+          case 'pembangunan':
+            break;
+          case 'batas':
+            for(let i=0; i<this.mapData.batas.length; i++) {
+               let data = this.mapData.batas[i];
+               let feature = this.mapLayout.filter(e => e.id == data.spatialId)[0];
+
+               geojson.features.push(feature);
+            }
+            break;
+          case 'potensi': 
+            for(let i=0; i<this.mapData.potensi.length; i++) {
+               let data = this.mapData.potensi[i];
+               let feature = this.mapLayout.filter(e => e.id == data.spatialId)[0];
+
+               geojson.features.push(feature);
+            }
+            break;
+       }
+
+       this.geojsonData = L.geoJSON(geojson, this.geojsonOptions).addTo(this.map);
+
+       return false;
+    }
+    
+    clearMap() {
+        this.geojsonData ? this.map.removeLayer(this.geojsonData) : null;
     }
 }
