7acd4dd3f1738947956e9ca4290f1682ade3cfb3
Octavianus Sinaga <octavianussinaga91@gmail.com>
2017-10-26 12:24:13 +0700

    default score

--- common/quality/statistics.py
+++ common/quality/statistics.py
@@ -228,7 +228,7 @@ def get_keuangan_statistics(cur, desa_id):
 			sum_quantity_rkp += result["perencanaan"]["rkp"]["quantity_"+rkp_name]["score"]
 		result["perencanaan"]["rkp"]["total_score_quality"] = sum_quality_rkp/6.0
 		result["perencanaan"]["rkp"]["total_score_quantity"] = sum_quantity_rkp/6.0
-		result["perencanaan"]["rkp"]["total_score"]=0.8*result["perencanaan"]["rkp"]["total_score_quality"] + 0.2*result["perencanaan"]["rkp"]["total_score_quantity"]
+		result["perencanaan"]["rkp"]["total_score"]=0.6*result["perencanaan"]["rkp"]["total_score_quality"] + 0.4*result["perencanaan"]["rkp"]["total_score_quantity"]
 
 	result["perencanaan"]["score"] = 0.1 * result["perencanaan"]["renstra"]["score"] + 0.4 * result["perencanaan"]["rpjm"]["total_score"] + 0.4 * result["perencanaan"]["rkp"]["total_score"] + 0.1 * result["perencanaan"]["last_modified_perencanaan"]["score"]
 
@@ -251,7 +251,7 @@ def get_keuangan_statistics(cur, desa_id):
 		rab_score_quality = get_scale(rab_quality_count, 15)
 		result["pengangaran"]["rab"]["quantity"]={"count":rab_quantity_count , "score":rab_score_quantity}
 		result["pengangaran"]["rab"]["quality"]={"count":rab_quality_count, "score":rab_score_quality}
-	result["pengangaran"]["score"] = 0.2 *result["pengangaran"]["rab"]["quality"]["score"] + 0.7 * result["pengangaran"]["rab"]["quantity"]["score"] + 0.1 * result["pengangaran"]["last_modified_penganggaran"]["score"]
+	result["pengangaran"]["score"] = 0.25 *result["pengangaran"]["rab"]["quality"]["score"] + 0.7 * result["pengangaran"]["rab"]["quantity"]["score"] + 0.05 * result["pengangaran"]["last_modified_penganggaran"]["score"]
 
 
 	#Calculate SPP
@@ -292,7 +292,7 @@ def get_keuangan_statistics(cur, desa_id):
 			sum_quality_spp+=data_score["quality"]["score"]
 		spp_quantity_avg=sum_quantity_spp/3.0
 		spp_quality_avg=sum_quality_spp/3.0
-		spp_score_datas= 0.9*spp_quantity_avg + 0.1*spp_quality_avg
+		spp_score_datas= 0.7*spp_quantity_avg + 0.3*spp_quality_avg
 		date_spp={}
 		spp_cur_=spp_cur["spp"]
 		#date_array=[r[3] for r in spp_cur]
@@ -302,7 +302,7 @@ def get_keuangan_statistics(cur, desa_id):
 		date_spp["score"]=get_scale(7 -(datetime.now() - date_spp["max"]).days , 7)
 		spp_score["last_modified"]=last_modified_spp
 		spp_score["date_spp"]=str(date_spp)
-		spp_score["score"]=0.1*last_modified_spp["score"] + 0.8*spp_score_datas + 0.1*date_spp["score"]
+		spp_score["score"]=0.05*last_modified_spp["score"] + 0.9*spp_score_datas + 0.05*date_spp["score"]
 	result["spp"]=spp_score
 
 	#Calculate Penerimaan
@@ -344,7 +344,7 @@ def get_keuangan_statistics(cur, desa_id):
 			sum_quality_penerimaan+=data_score["quality"]["score"]
 		penerimaan_quantity_avg=sum_quantity_penerimaan/2.0
 		penerimaan_quality_avg=sum_quality_penerimaan/2.0
-		penerimaan_score_datas= 0.8*penerimaan_quantity_avg + 0.2*penerimaan_quality_avg
+		penerimaan_score_datas= 0.7*penerimaan_quantity_avg + 0.3*penerimaan_quality_avg
 		date_tbp={}
 		tbp_cur=penerimaan_cur["tbp"]
 		#date_array=[r[3] for r in spp_cur]
@@ -354,7 +354,7 @@ def get_keuangan_statistics(cur, desa_id):
 		date_tbp["score"]=get_scale(7 -(datetime.now() - date_tbp["max"]).days , 7)
 		penerimaan["last_modified"]=last_modified_penerimaan
 		penerimaan["date_tbp"]=str(date_tbp)
-		penerimaan["score"]=0.1*last_modified_penerimaan["score"] + 0.8*penerimaan_score_datas + 0.1*date_tbp["score"]
+		penerimaan["score"]=0.05*last_modified_penerimaan["score"] + 0.9*penerimaan_score_datas + 0.05*date_tbp["score"]
 	result["penerimaan"]=penerimaan
 
 		#Calculate Total
