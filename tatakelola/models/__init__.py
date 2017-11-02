4e75a32fd50387cca1d88cd7df779cbcf113a3b9
Vidyapati <vidyapati@microvac.co.id>
2017-11-02 15:33:13 +0700

    Add data fetcher

--- tatakelola/models/__init__.py
+++ tatakelola/models/__init__.py
@@ -1,4 +1,5 @@
 from base import BaseModel
 from region import Region, RegionModelSchema, RegionCompleteModelSchema
 from geojson import Geojson, GeojsonModelSchema
+from data import Data, DataModelSchema
 from common.models import SdContent, SdContentSchema
