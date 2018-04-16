26e1ac98fd0c557a23ca2732ca640c329eb4d24e
Sideka Server <sideka@bp2dk.id>
2018-04-16 19:17:34 +0000

    update keuangan

--- keuangan/repository/progress_recapitulation.py
+++ keuangan/repository/progress_recapitulation.py
@@ -1,8 +1,15 @@
 from base import BaseRepository, BaseRegionRepository
 from keuangan.models import ProgressRecapitulation
+from keuangan.helpers import QueryHelper
 
 
 class ProgressRecapitulationRepository(BaseRepository, BaseRegionRepository):
     def __init__(self, db):
         self.db = db
         self.model = ProgressRecapitulation
+    
+    def get_region_years(self, region_id):
+        query = self.db.session.query(self.model)
+        return [ int(p.year) for p in query \
+            .filter(self.model.fk_region_id == region_id) \
+            .distinct(ProgressRecapitulation.year) ]
