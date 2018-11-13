42e546d54011a10333c2c86afad6b813186883ff
tesla-1st-code <firstcode2017@gmail.com>
2018-11-13 15:15:52 +0700

    add post score

--- tatakelola/controllers/post.py
+++ tatakelola/controllers/post.py
@@ -12,8 +12,8 @@ sideka_desa_repository = SidekaDesaRepository(db)
 
 @app.route('/post/<string:code>', methods=['GET'])
 def get_by_desa_code(code):
-    page_sort_params = QueryHelper.get_page_sort_params_from_request(request)
     data = []
+    page_sort_params = QueryHelper.get_page_sort_params_from_request(request)
     desas = sideka_desa_repository.get_by_code(code)
    
     for desa in desas:
@@ -22,5 +22,5 @@ def get_by_desa_code(code):
         if len(posts) > 0:
             result = SdPostScoresSchema(many=True).dump(posts)
             data.append(result.data)
-
+    
     return jsonify(data)
